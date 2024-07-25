5090e788ee365053d786b3b5fd79e616d4209514
zabao-qt <tagiabao.qt@gmail.com>
2024-07-25 15:40:19 +0700

    add: sign in and sign up

---
+++ FE/front-end/app/signin/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import SignIn from "../components/SignIn"
+
+function page() {
+  return (
+    <SignIn />
+  )
+}
+
+export default page
---
+++ FE/front-end/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignUp from "../components/SignUp"
+
+const page = () => {
+  return (
+    <SignUp />
+  )
+}
+
+export default page
